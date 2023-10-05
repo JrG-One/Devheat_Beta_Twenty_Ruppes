846488f8f341b3571129be31ebbf24d1e15e34ce
JrG-One <ojaswavarshney27@gmail.com>
2023-10-06 00:06:49 +0530

    2 Commit by JRG0ne

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -11,29 +11,22 @@ import {
   Stack,
   Link,
 } from "@chakra-ui/react";
-// import Logo from "../fitness.png";
 import { useNavigate } from "react-router-dom";
 import HealthStatsCard from "./HealthStatsCard";
 import ActivityCard from "./ActivityCard";
 import FatGraph from "./FatGraph";
-import Loading from "./Loader";
 
 const Dashboard = () => {
   const [fitnessData, setFitnessData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
   let result;
 
   useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      axios.get("https://http://localhost:3000/fetch-data").then((response) => {
-        // Handle the retrieved data
-        setFitnessData(response.data);
-        setIsLoading(false);
-      });
-    }, 3000);
+    axios.get("https://http://localhost:3000/fetch-data").then((response) => {
+      // Handle the retrieved data
+      setFitnessData(response.data);
+    });
   }, []);
 
   if (fitnessData) {
@@ -48,10 +41,10 @@ const Dashboard = () => {
   };
 
   const handleClick = () => {
-    navigate("/about"); // Replace "/another-page" with the desired URL
+    navigate("/about"); // Replace "/about" with the desired URL
   };
   const handleClick1 = () => {
-    navigate("/contact"); // Replace "/another-page" with the desired URL
+    navigate("/contact"); // Replace "/contact" with the desired URL
   };
 
   const glucose = fitnessData?.formattedData.map((item) => ({
@@ -90,131 +83,119 @@ const Dashboard = () => {
   );
 
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Box>
-          <Box
-            bg={colorMode === "light" ? "teal.500" : "pink.800"}
-            px={4}
-            py={2}
-            color="white"
-          >
-            <Flex align="center" justify="space-between">
-              <Flex align="center">
-                {/* <Image
-                  src={Logo}
-                  alt="Fitness Tracker Logo"
-                  width="40px"
-                  mr={3}
-                /> */}
-                <Text fontSize="xl" fontWeight="bold">
-                  Fitness Tracker
-                </Text>
-              </Flex>
-              <Flex align="center">
-                <Avatar
-                  size="sm"
-                  name="John Doe"
-                  src={fitnessData?.profilePhoto}
-                  mr={2}
-                />
-                <Text fontWeight="bold">{fitnessData?.userName}</Text>
-                <Button
-                  colorScheme={colorMode === "light" ? "white" : "blue"}
-                  variant="outline"
-                  size="sm"
-                  ml={4}
-                  onClick={handleLogout}
-                >
-                  Logout
-                </Button>
-                <Button
-                  colorScheme={colorMode === "light" ? "white" : "blue"}
-                  variant="outline"
-                  size="sm"
-                  ml={4}
-                  onClick={toggleColorMode}
-                >
-                  Toggle {colorMode === "light" ? "Dark" : "Light"} Mode
-                </Button>
-              </Flex>
-            </Flex>
-          </Box>
-          <Flex>
-            <Box
-              w="20%"
-              h="110vh"
-              p={4}
-              bg={colorMode === "light" ? "gray.100" : "gray.700"}
-            >
-              <Stack spacing={7}>
-                <Link
-                  href="#"
-                  color={colorMode === "light" ? "teal.500" : "white"}
-                  fontWeight="bold"
-                  _hover={{ textDecoration: "none" }}
-                  _focus={{ outline: "none" }}
-                  as="a"
-                  _notLast={{ mb: 2 }}
-                >
-                  Home
-                </Link>
-                <Link
-                  href="#"
-                  onClick={handleClick}
-                  color={colorMode === "light" ? "teal.500" : "white"}
-                  fontWeight="bold"
-                  _hover={{ textDecoration: "none" }}
-                  _focus={{ outline: "none" }}
-                  as="a"
-                  _notLast={{ mb: 2 }}
-                >
-                  About Us
-                </Link>
-                <Link
-                  href="#"
-                  onClick={handleClick1}
-                  color={colorMode === "light" ? "teal.500" : "white"}
-                  fontWeight="bold"
-                  _hover={{ textDecoration: "none" }}
-                  _focus={{ outline: "none" }}
-                  as="a"
-                  _notLast={{ mb: 2 }}
-                >
-                  Contacts
-                </Link>
-                <Link
-                  href="#"
-                  color={colorMode === "light" ? "teal.500" : "white"}
-                  fontWeight="bold"
-                  _hover={{ textDecoration: "none" }}
-                  _focus={{ outline: "none" }}
-                  as="a"
-                  _notLast={{ mb: 2 }}
-                >
-                  Services
-                </Link>
-              </Stack>
-            </Box>
-            <Flex direction="column" w="80%" p={4}>
-              <Stack spacing={4}>
-                <HealthStatsCard
-                  weight={maxWeight}
-                  height={maxHeight}
-                  BP={maxBPArray}
-                  step={StepCount}
-                  heart={heartrate}
-                />
-                <ActivityCard result={result} glucose={glucose} />
-                <FatGraph fat={fat} />
-              </Stack>
-            </Flex>
+    <Box>
+      <Box
+        bg={colorMode === "light" ? "teal.500" : "pink.800"}
+        px={4}
+        py={2}
+        color="white"
+      >
+        <Flex align="center" justify="space-between">
+          <Flex align="center">
+            <Text fontSize="xl" fontWeight="bold">
+              Fitness Tracker
+            </Text>
           </Flex>
+          <Flex align="center">
+            <Avatar
+              size="sm"
+              name="John Doe"
+              src={fitnessData?.profilePhoto}
+              mr={2}
+            />
+            <Text fontWeight="bold">{fitnessData?.userName}</Text>
+            <Button
+              colorScheme={colorMode === "light" ? "white" : "blue"}
+              variant="outline"
+              size="sm"
+              ml={4}
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+            <Button
+              colorScheme={colorMode === "light" ? "white" : "blue"}
+              variant="outline"
+              size="sm"
+              ml={4}
+              onClick={toggleColorMode}
+            >
+              Toggle {colorMode === "light" ? "Dark" : "Light"} Mode
+            </Button>
+          </Flex>
+        </Flex>
+      </Box>
+      <Flex>
+        <Box
+          w="20%"
+          h="110vh"
+          p={4}
+          bg={colorMode === "light" ? "gray.100" : "gray.700"}
+        >
+          <Stack spacing={7}>
+            <Link
+              href="#"
+              color={colorMode === "light" ? "teal.500" : "white"}
+              fontWeight="bold"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ outline: "none" }}
+              as="a"
+              _notLast={{ mb: 2 }}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              onClick={handleClick}
+              color={colorMode === "light" ? "teal.500" : "white"}
+              fontWeight="bold"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ outline: "none" }}
+              as="a"
+              _notLast={{ mb: 2 }}
+            >
+              About Us
+            </Link>
+            <Link
+              href="#"
+              onClick={handleClick1}
+              color={colorMode === "light" ? "teal.500" : "white"}
+              fontWeight="bold"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ outline: "none" }}
+              as="a"
+              _notLast={{ mb: 2 }}
+            >
+              Contacts
+            </Link>
+            <Link
+              href="#"
+              color={colorMode === "light" ? "teal.500" : "white"}
+              fontWeight="bold"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ outline: "none" }}
+              as="a"
+              _notLast={{ mb: 2 }}
+            >
+              Services
+            </Link>
+          </Stack>
         </Box>
-      )}
-    </>
+        <Flex direction="column" w="80%" p={4}>
+          <Stack spacing={4}>
+            <HealthStatsCard
+              weight={maxWeight}
+              height={maxHeight}
+              BP={maxBPArray}
+              step={StepCount}
+              heart={heartrate}
+            />
+            <ActivityCard result={result} glucose={glucose} />
+            <FatGraph fat={fat} />
+          </Stack>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
 
